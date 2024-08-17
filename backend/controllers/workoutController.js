e8c1fec31ad0c1b594ea27db48c3edfd9e8762b3
codelahiru <gayan.hatharasinghe@gmail.com>
2024-08-17 22:52:12 +0530

    lesson 6 cont.

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,8 +1,9 @@
 const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
 
 // function to get all workouts
 const getWorkouts = async (req, res) => {
-    const workouts = await workout.find({}).sort({createdAt: -1})
+    const workouts = await Workout.find({}).sort({createdAt: -1})
 
     res.status(200).json(workouts)
 }
@@ -10,6 +11,10 @@ const getWorkouts = async (req, res) => {
 // funcion to get a single workout
 const getWorkout = async (req, res) => {
     const { id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+            return res.status(404).json({error: 'No such workout'})
+    }
 
     const workout = await Workout.findById(id)
 
